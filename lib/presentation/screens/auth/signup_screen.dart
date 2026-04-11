200357b510dc698944fbd3e35d9231145d7582b6
DevExplorerr <saadj4775@gmail.com>
2026-04-11 20:48:42 +0500

    feat(auth): enhance sign-up screen with loading state and improve button functionality

--- lib/presentation/screens/auth/signup_screen.dart
+++ lib/presentation/screens/auth/signup_screen.dart
@@ -26,107 +26,111 @@ class SignUpScreen extends GetView<SignUpController> {
                 child: IntrinsicHeight(
                   child: Form(
                     key: controller.signupFormKey,
-                    child: Column(
-                      children: [
-                        const SizedBox(height: kToolbarHeight),
-                        const Spacer(),
-                        Text(
-                          'Join us to start searching',
-                          style: textTheme.headlineMedium,
-                        ),
-                        const SizedBox(height: 15),
-                        Text(
-                          'You can search doctors, book appointments and find medical aid effortlessly',
-                          textAlign: .center,
-                          style: textTheme.bodyMedium?.copyWith(
-                            color: AppColors.textSecondary,
-                          ),
-                        ),
-                        const SizedBox(height: 40),
-                        CustomTextField(
-                          controller: controller.nameController,
-                          hintText: "Full Name",
-                          prefixIcon: Icons.person_outline,
-                          keyboardType: .name,
-                          validator: (val) => (val?.isEmpty ?? true)
-                              ? "Name is required"
-                              : null,
-                        ),
-                        const SizedBox(height: 20),
-                        CustomTextField(
-                          controller: controller.emailController,
-                          hintText: "Email",
-                          prefixIcon: Icons.email_outlined,
-                          keyboardType: .emailAddress,
-                          validator: (val) => GetUtils.isEmail(val ?? '')
-                              ? null
-                              : "Enter a valid email",
-                        ),
-                        const SizedBox(height: 20),
-                        CustomTextField(
-                          controller: controller.passwordController,
-                          hintText: "Password",
-                          isPassword: true,
-                          prefixIcon: Icons.lock_outline,
-                          textInputAction: .done,
-                          keyboardType: .visiblePassword,
-                          validator: (val) => (val?.length ?? 0) >= 6
-                              ? null
-                              : "Minimum 6 characters",
-                        ),
-                        const SizedBox(height: 20),
-
-                        Obx(
-                          () => Row(
-                            children: [
-                              Checkbox(
-                                value: controller.isTermsAgreed.value,
-                                activeColor: AppColors.primary,
-                                onChanged: controller.toggleTerms,
+                    child: Obx(
+                      () => AbsorbPointer(
+                        absorbing: controller.isLoading.value,
+                        child: Column(
+                          children: [
+                            const SizedBox(height: kToolbarHeight),
+                            const Spacer(),
+                            Text(
+                              'Join us to start searching',
+                              style: textTheme.headlineMedium,
+                            ),
+                            const SizedBox(height: 15),
+                            Text(
+                              'You can search doctors, book appointments and find medical aid effortlessly',
+                              textAlign: .center,
+                              style: textTheme.bodyMedium?.copyWith(
+                                color: AppColors.textSecondary,
                               ),
-                              Expanded(
-                                child: Text(
-                                  'I agree with the Terms of Service & Privacy Policy',
-                                  style: textTheme.bodySmall,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
+                            ),
+                            const SizedBox(height: 40),
+                            CustomTextField(
+                              controller: controller.nameController,
+                              hintText: "Full Name",
+                              prefixIcon: Icons.person_outline,
+                              keyboardType: .name,
+                              validator: (val) => (val?.isEmpty ?? true)
+                                  ? "Name is required"
+                                  : null,
+                            ),
+                            const SizedBox(height: 20),
+                            CustomTextField(
+                              controller: controller.emailController,
+                              hintText: "Email",
+                              prefixIcon: Icons.email_outlined,
+                              keyboardType: .emailAddress,
+                              validator: (val) => GetUtils.isEmail(val ?? '')
+                                  ? null
+                                  : "Enter a valid email",
+                            ),
+                            const SizedBox(height: 20),
+                            CustomTextField(
+                              controller: controller.passwordController,
+                              hintText: "Password",
+                              isPassword: true,
+                              prefixIcon: Icons.lock_outline,
+                              textInputAction: .done,
+                              keyboardType: .visiblePassword,
+                              validator: (val) => (val?.length ?? 0) >= 6
+                                  ? null
+                                  : "Minimum 6 characters",
+                            ),
+                            const SizedBox(height: 20),
 
-                        const SizedBox(height: 30),
-
-                        CustomButton(
-                          text: controller.isLoading.value
-                              ? "Creating Account..."
-                              : "Sign Up",
-                          onTap: controller.signUp,
-                        ),
-
-                        const Spacer(flex: 2),
-
-                        Padding(
-                          padding: const .only(bottom: 40),
-                          child: GestureDetector(
-                            onTap: () => Get.back(),
-                            child: RichText(
-                              text: TextSpan(
-                                text: "Have an account? ",
-                                style: textTheme.bodyMedium,
+                            Obx(
+                              () => Row(
                                 children: [
-                                  TextSpan(
-                                    text: "Log in",
-                                    style: textTheme.bodyMedium?.copyWith(
-                                      color: AppColors.primary,
-                                      fontWeight: .w700,
+                                  Checkbox(
+                                    value: controller.isTermsAgreed.value,
+                                    activeColor: AppColors.primary,
+                                    onChanged: controller.toggleTerms,
+                                  ),
+                                  Expanded(
+                                    child: Text(
+                                      'I agree with the Terms of Service & Privacy Policy',
+                                      style: textTheme.bodySmall,
                                     ),
                                   ),
                                 ],
                               ),
                             ),
-                          ),
+
+                            const SizedBox(height: 30),
+
+                            CustomButton(
+                              text: "Sign Up",
+                              isLoading: controller.isLoading.value,
+                              onTap: controller.signUp,
+                            ),
+
+                            const Spacer(flex: 2),
+
+                            Padding(
+                              padding: const .only(bottom: 40),
+                              child: GestureDetector(
+                                onTap: () => Get.back(),
+                                child: RichText(
+                                  text: TextSpan(
+                                    text: "Have an account? ",
+                                    style: textTheme.bodyMedium,
+                                    children: [
+                                      TextSpan(
+                                        text: "Log in",
+                                        style: textTheme.bodyMedium?.copyWith(
+                                          color: AppColors.primary,
+                                          fontWeight: .w700,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                      ],
+                      ),
                     ),
                   ),
                 ),
--- lib/presentation/widgets/buttons/custom_button.dart
+++ lib/presentation/widgets/buttons/custom_button.dart
@@ -1,5 +1,6 @@
 import 'package:doctor_hunt/core/constants/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 class CustomButton extends StatelessWidget {
   final String text;
@@ -11,6 +12,7 @@ class CustomButton extends StatelessWidget {
   final double? borderRadius;
   final Color? textColor;
   final double? fontSize;
+  final bool isLoading;
 
   const CustomButton({
     super.key,
@@ -23,6 +25,7 @@ class CustomButton extends StatelessWidget {
     this.borderRadius,
     this.textColor,
     this.fontSize,
+    this.isLoading = false,
   });
 
   @override
@@ -36,32 +39,37 @@ class CustomButton extends StatelessWidget {
     final double responsiveFontSize =
         fontSize ?? (size.width * 0.045).clamp(14.0, 18.0);
 
-    return SizedBox(
-      width: width ?? .infinity,
-      height: responsiveHeight,
-      child: ElevatedButton(
-        style: ElevatedButton.styleFrom(
-          elevation: 0,
-          padding: const .symmetric(horizontal: 16),
-          backgroundColor: buttonColor ?? theme.primaryColor,
-          foregroundColor:
-              foregroundColor ?? AppColors.white.withValues(alpha: 0.1),
-          shape: RoundedRectangleBorder(
-            borderRadius: .circular(borderRadius ?? 12),
-          ),
-        ),
-        onPressed: onTap,
-        child: FittedBox(
-          fit: .scaleDown,
-          child: Text(
-            text,
-            style: theme.textTheme.titleMedium?.copyWith(
-              color: textColor ?? AppColors.white,
-              fontSize: responsiveFontSize,
+    return isLoading
+        ? LoadingAnimationWidget.staggeredDotsWave(
+            color: AppColors.primary,
+            size: 40,
+          )
+        : SizedBox(
+            width: width ?? .infinity,
+            height: responsiveHeight,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                elevation: 0,
+                padding: const .symmetric(horizontal: 16),
+                backgroundColor: buttonColor ?? theme.primaryColor,
+                foregroundColor:
+                    foregroundColor ?? AppColors.white.withValues(alpha: 0.1),
+                shape: RoundedRectangleBorder(
+                  borderRadius: .circular(borderRadius ?? 12),
+                ),
+              ),
+              onPressed: isLoading ? null : onTap,
+              child: FittedBox(
+                fit: .scaleDown,
+                child: Text(
+                  text,
+                  style: theme.textTheme.titleMedium?.copyWith(
+                    color: textColor ?? AppColors.white,
+                    fontSize: responsiveFontSize,
+                  ),
+                ),
+              ),
             ),
-          ),
-        ),
-      ),
-    );
+          );
   }
 }
